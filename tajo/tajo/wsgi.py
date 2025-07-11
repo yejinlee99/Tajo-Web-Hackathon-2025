700163c746781a1de7cdf0b4710c6948c729b97e
yejinlee99 <leeyejin991215@gmail.com>
2025-07-12 01:58:22 +0900

    Chore: wsgi.py 경로 수정...

--- tajo/tajo/wsgi.py
+++ tajo/tajo/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tajo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tajo.tajo.settings')
 
 application = get_wsgi_application()
