67e4fa9b6eda5938528b1d270a9c1daa053e55de
XPC-065\Administrator <leeyejin991215@gmail.com>
2025-07-20 20:41:50 +0900

    Feat: 호출 화면흐름 완성

--- call/views.py
+++ call/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.urls import path
+from . import views
 
 # Create your views here.
 from django.http import HttpResponse
@@ -6,3 +8,22 @@ from django.shortcuts import render
 
 def where_to(request):
     return render(request, 'call/where_to.html')
+
+def set_people(request):
+    return render(request, 'call/set_people.html')
+
+def waiting(request):
+    return render(request, 'call/waiting.html')
+
+def select_vehicle(request):
+    return render(request, 'call/select_vehicle.html')
+
+def assigned_vehicle(request):
+    return render(request, 'call/assigned_vehicle.html')
+
+def cancel_call(request):
+    return render(request, 'call/cancel_call.html')
+
+
+
+
---
+++ static/js/call/assigned_vehicle.js
@@ -0,0 +1,9 @@
+// 배차된 차량에 대한 모달 창
+
+window.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("arriveModal").style.display = "flex";
+});
+
+function closeModal() {
+  document.getElementById("arriveModal").style.display = "none";
+}
