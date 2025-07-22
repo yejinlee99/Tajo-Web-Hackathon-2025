9b207b2c8fef99c99873af97e5f903b21d2de272
XPC-049\Administrator <leeyejin991215@gmail.com>
2025-07-22 21:42:33 +0900

    Feat: call 호출 화면 개선 css 및 js 수정

--- static/js/call/assigned_vehicle.js
+++ static/js/call/assigned_vehicle.js
@@ -1,9 +1,34 @@
-// 배차된 차량에 대한 모달 창
 
 window.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("arriveModal").style.display = "flex";
+  const cancelBtn = document.querySelector(".cancel-btn");
+  if (cancelBtn) {
+    cancelBtn.addEventListener("click", () => {
+      document.getElementById("cancelModal").style.display = "flex";
+    });
+  }
+
+  // 모달 보이기
+  const arriveModal = document.getElementById("arriveModal");
+  if (arriveModal) {
+    arriveModal.style.display = "flex";
+  }
 });
 
 function closeModal() {
-  document.getElementById("arriveModal").style.display = "none";
+  const arriveModal = document.getElementById("arriveModal");
+  if (arriveModal) {
+    arriveModal.style.display = "none";
+  }
+  document.querySelector(".assigned-main-content")?.classList.remove("blur-when-modal");
+}
+
+function closeCancelModal() {
+  const cancelModal = document.getElementById("cancelModal");
+  if (cancelModal) {
+    cancelModal.style.display = "none";
+  }
+}
+
+function goHome() {
+  window.location.href = "/";  // Django 템플릿 태그는 HTML에서 넣어줘야 함
 }
